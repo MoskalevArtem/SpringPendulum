9f23c6de480552bff1823e06348d1f15bee8a612
MoskalevArtem <154740201+MoskalevArtem@users.noreply.github.com>
2024-01-03 20:17:54 +0700

    Add files via upload

--- src/main/java/model/Pendulum.java
+++ src/main/java/model/Pendulum.java
@@ -1,73 +1,108 @@
 package model;
 
-import controller.Controller;
 import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleObjectProperty;
 import javafx.beans.value.ChangeListener;
-import javafx.scene.canvas.GraphicsContext;
-
+import java.util.ArrayList;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class Pendulum {
-    private static final int SPRING_LENGTH = 400;// длина пружины без груза
-    private final AtomicInteger angle = new AtomicInteger();
-    private final AtomicInteger tension = new AtomicInteger();
-    private ObjectProperty<PendulumParams> params = new SimpleObjectProperty<>();
-    private Thread processorThread;
-    private Cords areaSize;
-    private boolean shutdown = false;
+    private static final double SPRING_LENGTH = 300;// длина пружины без груза
+    private static final double K = 1;//жёсткость пружины
+    private static final double M = 1;//масса груза
+    private static final double G = 9.8;//ускорение свободного падения
+    private final AtomicInteger angle = new AtomicInteger();//угол отклонения груза
+    private final AtomicInteger tension = new AtomicInteger();//удлинение пружины в %
+    private ObjectProperty<PendulumParams> params = new SimpleObjectProperty<>();// объект для записи координат и угла
+    private Thread processorThread;//поток
+    private Cords areaSize;// размеры окна
+    private boolean shutdown = false;//состоянение потока
 
 
     public Pendulum(int angle, int tension, double width, double height) {
         setAngle(angle);
         setTension(tension);
-
-        double startX = width/2 + (SPRING_LENGTH+tension) * Math.sin(angle);//координата х центра масс груза
-        double startY = height/10+ (SPRING_LENGTH+tension)* Math.cos(angle);//координата у центра масс груза
-        params.set(new PendulumParams(new Cords(startX, startY), angle));
         areaSize = new Cords(width, height);
     }
-    public void startThread()
+    public void startThread()//запуск потока
     {
         processorThread = new Thread(this::startProcess);
         processorThread.start();
     }
 
     private void startProcess() {
-
-        double K = 1;//жёсткость пружины
-        double M = 1;//масса груза
-        double G = 9.8;//ускорение свободного падения
+        double fi0 = (double) angle.get() * Math.PI / 180;//начальный угол в радианах
+        double r1 = (double) tension.get() * SPRING_LENGTH / 100 + SPRING_LENGTH;//начальная длина пружины
+        double x = areaSize.x() / 2 + r1 * Math.sin(fi0);//координата х центра масс груза
+        double y = areaSize.y() / 10 + r1 * Math.cos(fi0);//координата у центра масс груза
+        double samplingTime = 0.05;//время между кадрами
+        double Vx = 0;//х состовляющая скорости груза
+        double Vy = 0;//у состовляющая скорости груза
+        double Ax = 0;//х состовляющая ускорения груза
+        double Ay = 0;//у состовляющая ускорения груза
+        ArrayList x0 = new ArrayList<Double>();// список, в который будут записываться х координата груза
+        ArrayList y0 = new ArrayList<Double>();// список, в который будут записываться у координата груза
         long startTime = System.currentTimeMillis();
-        long lastUpdate = -1;
-        double fi0 = (double) angle.get() * Math.PI / 180;
-        double r1 = (double) tension.get() * SPRING_LENGTH / 100;
-
         while (!shutdown) {
-            long currentTime = System.currentTimeMillis();
-            if (currentTime - lastUpdate < 50)
-                continue;
-            long t = (currentTime - startTime) / 100;
-            double r = SPRING_LENGTH + r1*Math.cos(Math.sqrt(M /K) * t/4)*Math.exp(-0.01*t/4);//зависимость длинны пружины от времени
-            double fi = fi0*Math.cos(Math.sqrt(G/SPRING_LENGTH) * t/4)*Math.exp(-0.01*t/4);//зависимость угла от времени
-            double x = areaSize.x() / 2 + r * Math.sin(fi);//координата х центра масс груза
-            double y = areaSize.y() / 10 + r * Math.cos(fi);//координата у центра масс груза
-            params.set(new PendulumParams(new Cords(x, y), fi));
+            long currentTime = System.currentTimeMillis()-startTime;
+            if (currentTime % (samplingTime*1000) == 0) {
+                x0.add(x);
+                y0.add(y);
+                Vx = getVx(x0, y0, samplingTime);
+                Vy = getVy(x0, y0, samplingTime);
+                Ax = getAx(x, y);
+                Ay = getAy(x, y);
+                x += (Vx * samplingTime + Ax * Math.pow(samplingTime,2) / 2);
+                y += (Vy * samplingTime + Ay * Math.pow(samplingTime,2) / 2);
+                double fi = getFi(x, y);
+                params.set(new PendulumParams(new Cords(x, y), fi));
+            }
         }
     }
-    public void setAngle(int angle) {
+    public double getLengthening(double x, double y) {//удлиннение пружины в точке (x,y)
+        return Math.sqrt(Math.pow((x-areaSize.x()/2),2)+Math.pow(y-areaSize.y()/10,2))-SPRING_LENGTH;
+    }
+    public double getFi(double x, double y){//угол отконения груза в точке (x,y)
+        return Math.atan((x-(areaSize.x()/2))/(y-(areaSize.y()/10)));
+    }
+    public double getAx(double x, double y){//х состовляющая ускорения в точке (x,y)
+        double fi = getFi(x,y);
+        return -K* getLengthening(x,y)*Math.sin(fi)/M;
+    }
+    public double getAy(double x, double y){//у состовляющая ускорения в точке (x,y)
+        double fi = getFi(x,y);
+        return G - K* getLengthening(x,y)*Math.cos(fi)/M;
+    }
+    public double getVx(ArrayList x, ArrayList y,double h) {//нахождение Vx в точке (x,y)
+        //методом трапеций интегрируем ускорение по х
+        double sum = 0;
+        for (int i = 1; i < x.size(); i++)
+            sum += (getAx((Double) x.get(i),(Double) y.get(i)) +
+                    getAx((Double) x.get(i-1), (Double) y.get(i-1))) / 2 * h;
+        return sum;
+    }
+    public double getVy(ArrayList x, ArrayList y,double h) {//нахождение Vу в точке (x,y)
+        //методом трапеций интегрируем ускорение по у
+        double sum = 0;
+        for (int i = 1; i < x.size(); i++)
+            sum += (getAy((Double) x.get(i), (Double) y.get(i)) +
+                    getAy((Double) x.get(i-1), (Double) y.get(i-1))) / 2 * h;
+        return sum;
+    }
+
+    public void setAngle(int angle) {//установка начального угла отклонения груза
         this.angle.set(angle);
     }
 
-    public void setTension(int tension) {
+    public void setTension(int tension) {//установка начального растяжения пружины в %
         this.tension.set(tension);
     }
-    public void shutdownThread()
+    public void shutdownThread()//метод для усыпления потока
     {
         shutdown = true;
     }
 
-    public void addOnCordsListener(ChangeListener<? super PendulumParams> listener) {
+    public void addOnCordsListener(ChangeListener<? super PendulumParams> listener) {//добавление слушателя
         params.addListener(listener);
     }
 
